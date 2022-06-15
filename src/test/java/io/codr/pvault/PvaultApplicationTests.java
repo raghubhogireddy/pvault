587b5c5e86a5bb18d23652a2beb9449fada124e1
raghuram bhogireddy <raghurambhogireddy@gmail.com>
2022-06-15 08:26:59 +0530

    codebase

---
+++ src/test/java/io/codr/pvault/PvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codr.pvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
